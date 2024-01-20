e9f756d4af9b69930aa34f80abb5459028946365
olimov Amir <olimov.amir@mail.ru>
2024-01-20 09:02:06 +0300

    changed Class Person

--- Models/Person.cs
+++ Models/Person.cs
@@ -2,6 +2,22 @@
 {
     public abstract class Person
     {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
 
+        public Person(string name, string lastName, DateTime birthday)
+        {
+
+            Name = name;
+            LastName = lastName;
+            Birthday = birthday;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {LastName}, {Birthday.ToShortDateString()}";
+        }
     }
 }
