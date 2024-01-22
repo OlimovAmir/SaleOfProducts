f60d27d057a280a410677dd1255ef6187d89c291
olimov Amir <olimov.amir@mail.ru>
2024-01-22 10:31:57 +0300

    fix

---
+++ Controllers/WarehouseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleOfProducts.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WarehouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
