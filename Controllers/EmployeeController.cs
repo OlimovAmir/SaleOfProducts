f452b1753facd60839610cae4a28b8a020738e07
olimov Amir <olimov.amir@mail.ru>
2024-01-22 10:05:01 +0300

    fix

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -14,13 +14,63 @@ namespace SaleOfProducts.Controllers
             _logger = logger;
         }
 
+        private static List<Employee> workers = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "Amir", LastName = "Olimov" },
+            new Employee { Id = 2, Name = "Parvina", LastName = "Olimova" },
+            new Employee { Id = 3, Name = "Muborak", LastName = "Olimova" }
+        };
+
         [HttpGet]
         public IEnumerable<Employee> Get()
         {
-            var workers = new List<Employee>();
-            workers.Add(new Employee() { Name  = "Test1"});
-            workers.Add(new Employee() { Name  = "Test2" });
             return workers;
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetById(int id)
+        {
+            var employee = workers.Find(e => e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> Create(Employee employee)
+        {
+            employee.Id = workers.Count + 1;
+            workers.Add(employee);
+            return CreatedAtAction(nameof(GetById), new { id = employee.Id }, employee);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Employee updatedEmployee)
+        {
+            var existingEmployee = workers.Find(e => e.Id == id);
+            if (existingEmployee == null)
+            {
+                return NotFound();
+            }
+
+            existingEmployee.Name = updatedEmployee.Name;
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var employee = workers.Find(e => e.Id == id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            workers.Remove(employee);
+            return NoContent();
+        }
+
     }
 }
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,15 +1,20 @@
-﻿namespace SaleOfProducts.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaleOfProducts.Models
 {
     public abstract class Person
     {
-        public Guid Id { get; } = Guid.NewGuid();
+        public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string LastName { get; set; }
         public DateTime Birthday { get; set; }
 
-        public Person(string name, string lastName, DateTime birthday)
+        public Person(int id, string name, string lastName, DateTime birthday)
         {
-
+            Id  = id;
             Name = name;
             LastName = lastName;
             Birthday = birthday;
