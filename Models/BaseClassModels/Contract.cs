d80e67b37093a1e4beef4b3a4c79f6acfd045d2a
olimov Amir <olimov.amir@mail.ru>
2024-01-23 17:50:57 +0300

    fix

--- Models/BaseClassModels/Contract.cs
+++ Models/BaseClassModels/Contract.cs
@@ -2,5 +2,12 @@
 {
     public abstract class Contract
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ContractId { get; set; }
+        public DateTime Date { get; set; }
+        public string ContractType { get; set; }
+
     }
 }
