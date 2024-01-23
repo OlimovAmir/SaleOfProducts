572b81543ea0b191a2d450d8a3075df714513424
olimov Amir <olimov.amir@mail.ru>
2024-01-23 14:42:10 +0300

    fix

---
+++ Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleOfProducts.Controllers
+{
+    public class UserController : Controller
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
