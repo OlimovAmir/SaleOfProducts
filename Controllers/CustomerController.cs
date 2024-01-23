38ed0ac00b5665aa5b3808ac7c4636c34a00823b
olimov Amir <olimov.amir@mail.ru>
2024-01-23 13:51:54 +0300

    created controller Customer

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : Controller
+    {
+        private static List<Customer> customer = new List<Customer>
+        {
+            new Customer {
+                Id = 1,
+                Name = "A client from Russia",
+                Status = "LLC",
+                Address = "Russia",
+                INN = 533328140,
+                Phone = 928790000
+            },
+
+            new Customer {
+                Id = 2,
+                Name = "A client from Uzbekistan",
+                Status = "LLC",
+                Address = "Uzbekistan",
+                INN = 519928145,
+                Phone = 927722241
+            },
+
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Customer>> Get()
+        {
+            return Ok(customer);
+        }
+
+    }
+}
