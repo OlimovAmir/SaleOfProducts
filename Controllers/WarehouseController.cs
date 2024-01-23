c6d73cb4d876d67ac7def27e829e426c688dbeee
olimov Amir <olimov.amir@mail.ru>
2024-01-23 12:40:50 +0300

    fix

--- Controllers/WarehouseController.cs
+++ Controllers/WarehouseController.cs
@@ -12,9 +12,9 @@ namespace SaleOfProducts.Controllers
         private static List<Warehouse> warehouse = new List<Warehouse>
         
         {
-            new Warehouse { Id = 1, Name = "Product A"},
-            new Warehouse { Id = 2, Name = "Product B"},
-            new Warehouse { Id = 3, Name = "Product C"}
+            new Warehouse { Id = 1, Name = "Main warehouse"},
+            new Warehouse { Id = 2, Name = "Spare parts warehouse"},
+            new Warehouse { Id = 3, Name = "Lumber warehouse"}
         };
 
         [HttpGet]
--- Models/Warehouse.cs
+++ Models/Warehouse.cs
@@ -4,5 +4,25 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+
+
+        public Warehouse(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+
+        public Warehouse()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"\tName:{Name}\tAddress: {Address}";
+        }
     }
+
+    
 }
