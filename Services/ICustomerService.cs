71306001965e1d9b7a5b6cad916290fc1645d47a
olimov Amir <olimov.amir@mail.ru>
2024-01-24 17:44:29 +0300

    created Interface Customer

---
+++ Services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetAll();
+        Customer GetById(Guid id);
+        string Create(Customer worker);
+        string Update(Guid id, Customer item);
+        string Delete(Guid id);
+    }
+}
