c803cd3954240a106c6929c2d194173ba353d2b3
olimov Amir <olimov.amir@mail.ru>
2024-01-24 10:16:50 +0300

    fix

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -5,6 +5,12 @@ namespace SaleOfProducts.Models
 {
     public class Supplier : Company
     {
-       
+        public SupplierStatus State { get; set; }
+    }
+
+    public enum SupplierStatus
+    {
+        Orginization,
+        Persion
     }
 }
