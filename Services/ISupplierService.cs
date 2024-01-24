2921537e41d7cc92c82395760ec0f89fa886759b
olimov Amir <olimov.amir@mail.ru>
2024-01-24 10:20:45 +0300

    created interface

---
+++ Services/ISupplierService.cs
@@ -0,0 +1,13 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public interface ISupplierService
+    {
+        IEnumerable<Supplier> GetAll();
+        Supplier GetById(Guid id);
+        string Create(Supplier worker);
+        string Update(Guid id, Supplier item);
+        string Delete(Guid id);
+    }
+}
