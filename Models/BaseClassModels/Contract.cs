fb7cb403db3a9cf8e5820a820c10ed16c73968d4
olimov Amir <olimov.amir@mail.ru>
2024-01-24 09:19:47 +0300

    fix

--- Models/BaseClassModels/Contract.cs
+++ Models/BaseClassModels/Contract.cs
@@ -1,11 +1,16 @@
-﻿namespace SaleOfProducts.Models.BaseClassModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaleOfProducts.Models.BaseClassModels
 {
     public abstract class Contract
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string Name { get; set; }
+
+        [Required]
+        public string Number { get; set; }
         public string Description { get; set; }
         public string ContractId { get; set; }
+        [Required]
         public DateTime Date { get; set; }
         public string ContractType { get; set; }
 
