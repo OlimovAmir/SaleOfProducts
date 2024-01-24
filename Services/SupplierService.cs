8c438cf82b98328a69f69b8ffc5bd30f2a3f284a
olimov Amir <olimov.amir@mail.ru>
2024-01-24 10:26:53 +0300

    fix

---
+++ Services/SupplierService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class SupplierService : ISupplierService
+    {
+        public string Create(Supplier worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Supplier> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Supplier GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, Supplier item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
