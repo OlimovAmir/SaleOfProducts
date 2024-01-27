5e47ba3bad18974f03f354ce45ce0b47a129fffa
olimov Amir <olimov.amir@mail.ru>
2024-01-27 10:54:01 +0300

    fix

--- Repositories/MemoryRepositiry.cs
+++ Repositories/MemoryRepositiry.cs
@@ -1,6 +1,34 @@
-﻿namespace SaleOfProducts.Repositories
+﻿using SaleOfProducts.Models.BaseClassModels;
+
+namespace SaleOfProducts.Repositories
 {
-    public class MemoryRepositiry
+    public class MemoryRepositiry<T> : IMemoryRepositiry<T> where T : BaseEntity
     {
+        public bool Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
 }
