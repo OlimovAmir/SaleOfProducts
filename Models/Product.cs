2cc8951893c2c5d017c0719efce82e468d6a0953
olimov Amir <olimov.amir@mail.ru>
2024-01-29 14:05:44 +0300

    Filling in the Product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -2,5 +2,16 @@
 {
     public class Product
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        // Property for linking to the Unit model
+        public Unit Unit { get; set; }
+
+        // A foreign key for communication with the Unit model
+        public Guid UnitId { get; set; }
+
+
     }
 }
--- Models/Unit.cs
+++ Models/Unit.cs
@@ -2,5 +2,7 @@
 {
     public class Unit
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
     }
 }
