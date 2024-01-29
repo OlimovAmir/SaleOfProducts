23de0e264d9d3e5956dd684d4ec7a9aabe312675
olimov Amir <olimov.amir@mail.ru>
2024-01-29 14:14:26 +0300

    fix

--- Models/Unit.cs
+++ Models/Unit.cs
@@ -4,5 +4,22 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
+
+
+        // Constructor
+        public Unit(string name)
+        {
+            Name = name;
+        }
+
+        public Unit()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
 }
