d4f5979123000914ef1defcb3e0ab5a0454de096
olimov Amir <olimov.amir@mail.ru>
2024-01-29 14:11:21 +0300

    fix

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,6 +12,19 @@
         // A foreign key for communication with the Unit model
         public Guid UnitId { get; set; }
 
+        // Constructor
+        public Product(string name, decimal price, Unit unit)
+        {
+            Name = name;
+            Price = price;
+            Unit = unit;
+            UnitId = unit.Id; // Assuming you want to set the foreign key based on the Unit's Id
+        }
 
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Price} {UnitId}";
+        }
     }
 }
