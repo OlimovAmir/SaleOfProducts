f4030ab6f8e08feef0c436da1a7de896493f767f
olimov Amir <olimov.amir@mail.ru>
2024-01-30 14:03:39 +0300

    created class CashExpense

---
+++ Models/CashExpense.cs
@@ -0,0 +1,22 @@
+﻿using SaleOfProducts.Models.BaseClassModels;
+
+namespace SaleOfProducts.Models
+{
+    public class CashExpense : CashTransaction
+    {
+        // Дополнительные свойства для учета расходов, если необходимо
+        public string Category { get; set; }
+
+        // Конструктор
+        public CashExpense(double amount, string description, string category)
+            : base(amount, description)
+        {
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Category: {Category}";
+        }
+    }
+}
