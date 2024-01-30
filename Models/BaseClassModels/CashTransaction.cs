14d3e26488549eb0dfe3e529663d0480330924c9
olimov Amir <olimov.amir@mail.ru>
2024-01-30 14:00:55 +0300

    fix

--- Models/BaseClassModels/CashTransaction.cs
+++ Models/BaseClassModels/CashTransaction.cs
@@ -2,5 +2,21 @@
 {
     public abstract class CashTransaction
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime TransactionDate { get; set; } = DateTime.Now;
+        public double Amount { get; set; }
+        public string Description { get; set; }
+
+        // Конструктор
+        public CashTransaction(double amount, string description)
+        {
+            Amount = amount;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {TransactionDate}: {Description}, Amount: {Amount}";
+        }
     }
 }
