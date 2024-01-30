74952ccdbd8d0a2bd649aa597df456a8c32d8ec9
olimov Amir <olimov.amir@mail.ru>
2024-01-30 14:04:59 +0300

    created class CashIncome

---
+++ Models/CashIncome.cs
@@ -0,0 +1,22 @@
+﻿using SaleOfProducts.Models.BaseClassModels;
+
+namespace SaleOfProducts.Models
+{
+    public class CashIncome : CashTransaction
+    {
+        // Дополнительные свойства для учета приходов, если необходимо
+        public string Source { get; set; }
+
+        // Конструктор
+        public CashIncome(double amount, string description, string source)
+            : base(amount, description)
+        {
+            Source = source;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, Source: {Source}";
+        }
+    }
+}
