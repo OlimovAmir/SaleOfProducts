d1e33ff8477ce38f21e8475f2f077a99ee5fd48c
olimov Amir <olimov.amir@mail.ru>
2024-01-31 08:39:04 +0300

    created interface ICashExoense

---
+++ Services/CashExpense.cs
@@ -0,0 +1,31 @@
+﻿
+namespace SaleOfProducts.Services
+{
+    public class CashExpense : ICashExpense
+    {
+        public string Create(CashExpense worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CashExpense> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CashExpense GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, CashExpense item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/ICashExpense.cs
@@ -0,0 +1,13 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public interface ICashExpense
+    {
+        IEnumerable<CashExpense> GetAll();
+        CashExpense GetById(Guid id);
+        string Create(CashExpense worker);
+        string Update(Guid id, CashExpense item);
+        string Delete(Guid id);
+    }
+}
