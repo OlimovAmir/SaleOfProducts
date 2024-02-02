44beb7e40cef4d45457f224cdfeaea310cb34e37
olimov Amir <olimov.amir@mail.ru>
2024-02-02 12:24:45 +0300

    fix

---
+++ Services/CashIncomeService.cs
@@ -0,0 +1,41 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class CashIncomeService : ICashIncomeService
+    {
+        static Dictionary<Guid, CashIncome> Items = new Dictionary<Guid, CashIncome>();
+        public string Create(CashIncome item)
+        {
+            if (string.IsNullOrEmpty(item.Description))
+            {
+                return "The name cannot be empty";
+            }
+            else
+            {
+                Items.Add(item.Id, item);
+                return $"Created new item with this ID: {item.Id}";
+            }
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CashIncome> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CashIncome GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, CashIncome item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
