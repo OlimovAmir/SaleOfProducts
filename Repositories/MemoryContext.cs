a2cd91ed634cef30ee08cf3eb9d7d1d4fabffce2
olimov Amir <olimov.amir@mail.ru>
2024-02-05 21:16:47 +0300

    PostgreSQL

--- Repositories/MemoryContext.cs
+++ Repositories/MemoryContext.cs
@@ -5,6 +5,7 @@ namespace SaleOfProducts.Repositories
 {
     public class MemoryContext:DbContext
     {
+
         public MemoryContext(DbContextOptions<MemoryContext> options)
             : base(options)
         {
@@ -12,5 +13,6 @@ namespace SaleOfProducts.Repositories
             Database.EnsureCreated();
         }
         public DbSet<Client> Clients { get; set; }
+        public DbSet<CashExpense> CashExpenses { get; set; }
     }
 }
