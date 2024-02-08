c4797d4a68e9124705e24a5b7130482fb8bb6823
olimov Amir <olimov.amir@mail.ru>
2024-02-08 18:00:14 +0300

    Add migration

--- Migrations/20240208145905_EmployeeMigration.Designer.cs
+++ Migrations/20240208145905_EmployeeMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SaleOfProducts.Repositories;
@@ -11,9 +12,11 @@ using SaleOfProducts.Repositories;
 namespace SaleOfProducts.Migrations
 {
     [DbContext(typeof(MemoryContext))]
-    partial class MemoryContextModelSnapshot : ModelSnapshot
+    [Migration("20240208145905_EmployeeMigration")]
+    partial class EmployeeMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
