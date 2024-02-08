b5adbf6137bde75f955d2dc8fe03406b1e73b825
olimov Amir <olimov.amir@mail.ru>
2024-02-08 16:29:41 +0300

    created file EmployeeService

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public string Create(Employee worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, Employee item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
