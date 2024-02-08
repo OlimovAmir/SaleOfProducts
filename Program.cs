461f61aaa0ea31d3ab0f90ac2851476a5130bcdc
olimov Amir <olimov.amir@mail.ru>
2024-02-08 17:21:04 +0300

    fix bag

--- Program.cs
+++ Program.cs
@@ -13,22 +13,18 @@ namespace SaleOfProducts
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<MemoryContext>(options =>
-           options.UseNpgsql(builder.Configuration.GetConnectionString("DbPostgres")));
-
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DbPostgres")));
 
             // Add services to the container.
 
             builder.Services.AddControllersWithViews();
-
-
-            //builder.Services.AddScoped<MemoryContext>();
+                        
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddScoped<ICashExpenseService, CashExpenseService>();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddSingleton<ISupplierService, SupplierService>();
             builder.Services.AddSingleton<ICustomerService, CustomerService>();
-            
             
             builder.Services.AddSingleton(typeof(IMemoryRepository<>), typeof(MemoryRepository<>));
 
