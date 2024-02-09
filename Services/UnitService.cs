66622aec0fec9af7ba1ccad81715f5686eb472b1
olimov Amir <olimov.amir@mail.ru>
2024-02-09 18:51:15 +0300

    has been created file UnitService

---
+++ Services/UnitService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class UnitService : IUnitService
+    {
+        public string Create(Unit worker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Unit> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Unit GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, Unit item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
