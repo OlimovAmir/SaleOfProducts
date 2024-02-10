91198ec6622824d4deec57712dcf3a76b64a43b5
olimov Amir <olimov.amir@mail.ru>
2024-02-10 20:14:03 +0300

    has been created file class PositionServise

---
+++ Services/PositionServise.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class PositionServise : IPositionServise
+    {
+        public string Create(Position item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Position> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Position GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, Position item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
