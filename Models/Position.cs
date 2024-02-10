0ee170f3ecdf2a9c1a23eeca98f581828473bd90
olimov Amir <olimov.amir@mail.ru>
2024-02-10 20:00:13 +0300

    has been created class Position

---
+++ Models/Position.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SaleOfProducts.Models
+{
+    public class Position
+    {
+        public Guid Id { get; }
+
+        [Required]
+        public string Title { get; set; } 
+        public string Description { get; set; }
+
+        public Position(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public Position()
+        {
+            
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Title} {Description}";
+        }
+
+    }
+}
