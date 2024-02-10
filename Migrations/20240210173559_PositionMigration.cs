8e0c2deaaf9628eaa6c55be369ee24b12eb88ee0
olimov Amir <olimov.amir@mail.ru>
2024-02-10 20:40:36 +0300

    has been created migration and table in dataBase

---
+++ Migrations/20240210173559_PositionMigration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaleOfProducts.Migrations
+{
+    /// <inheritdoc />
+    public partial class PositionMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Employees",
+                newName: "SurName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Employees",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Positions");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "SurName",
+                table: "Employees",
+                newName: "LastName");
+        }
+    }
+}
