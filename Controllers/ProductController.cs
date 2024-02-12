9a021eb4b18004427a23c2760222f0ac50c0911b
olimov Amir <olimov.amir@mail.ru>
2024-02-12 15:13:56 +0300

    has been created file controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProducts.Models;
+using SaleOfProducts.Services;
+
+namespace SaleOfProducts.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("AllItems")]
+        public IEnumerable<Product> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("GetItemById")]
+        public Product Get(Guid id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpPost("Create")]
+        public string Post([FromBody] Product item)
+        {
+            return _service.Create(item);
+        }
+
+        [HttpPut("Update")]
+        public string Put([FromQuery] Guid id, [FromBody] Product item)
+        {
+            return _service.Update(id, item);
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete([FromQuery] Guid id)
+        {
+            return _service.Delete(id);
+        }
+    }
+}
