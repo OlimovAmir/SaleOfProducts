9946f4b7d5097f8b7d989668778c5adf766e0fd7
olimov Amir <olimov.amir@mail.ru>
2024-02-12 14:56:00 +0300

    has been created file class ProductService

---
+++ Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class ProductService : IProductService
+    {
+        public string Create(Product item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, Product item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
