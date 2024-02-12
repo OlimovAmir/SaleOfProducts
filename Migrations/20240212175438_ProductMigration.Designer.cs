40697d9a1f5f4d836a4b7934f2af0e018b8d80bc
olimov Amir <olimov.amir@mail.ru>
2024-02-12 20:55:14 +0300

    has been created migration

--- Migrations/20240212175438_ProductMigration.Designer.cs
+++ Migrations/20240212175438_ProductMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SaleOfProducts.Infrastructure;
@@ -11,9 +12,11 @@ using SaleOfProducts.Infrastructure;
 namespace SaleOfProducts.Migrations
 {
     [DbContext(typeof(MemoryContext))]
-    partial class MemoryContextModelSnapshot : ModelSnapshot
+    [Migration("20240212175438_ProductMigration")]
+    partial class ProductMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
