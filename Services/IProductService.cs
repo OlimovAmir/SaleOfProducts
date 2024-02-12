9838ae78359d40fda6a6471e2dc9136b4d2919ac
olimov Amir <olimov.amir@mail.ru>
2024-02-12 14:54:32 +0300

    has been created file IProductService

---
+++ Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public interface IProductService
+    {
+        IEnumerable<Product> GetAll();
+        Product GetById(Guid id);
+        string Create(Product item);
+        string Update(Guid id, Product item);
+        string Delete(Guid id);
+    }
+}
