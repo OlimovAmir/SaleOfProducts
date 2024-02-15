18cb783e486a9f240e22fa6458abc17ee187d5ff
olimov Amir <olimov.amir@mail.ru>
2024-02-15 14:26:07 +0300

    has been created method Delete

--- Services/SupplierService.cs
+++ Services/SupplierService.cs
@@ -27,12 +27,14 @@ namespace SaleOfProducts.Services
 
         public string Delete(Guid id)
         {
-            var _item = Items.SingleOrDefault(w => w.Key == id).Value;
-            if (_item is null)
+            var itemToDelete = _dbContext.Suppliers.Find(id);
+            if (itemToDelete == null)
             {
                 return "Item not found";
             }
-            Items.Remove(id);
+
+            _dbContext.Suppliers.Remove(itemToDelete);
+            _dbContext.SaveChanges();
 
             return "Item deleted";
         }
