a886b530c5a4cd5bf9b7418f30dfa2fc41de5278
olimov Amir <olimov.amir@mail.ru>
2024-02-18 16:21:32 +0300

    fix

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SaleOfProducts.Models;
+using SaleOfProducts.Services;
 
 namespace SaleOfProducts.Controllers
 {
@@ -8,6 +9,42 @@ namespace SaleOfProducts.Controllers
     public class UserController : Controller
     {
 
-        
+        readonly ICashExpenseService _service;
+
+        public CashExpenseController(ICashExpenseService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("AllItems")]
+        public IEnumerable<CashExpense> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("GetItemById")]
+        public CashExpense Get(Guid id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpPost("Create")]
+        public string Post([FromBody] CashExpense item)
+        {
+            return _service.Create(item);
+        }
+
+        [HttpPut("Update")]
+        public string Put([FromQuery] Guid id, [FromBody] CashExpense item)
+        {
+            return _service.Update(id, item);
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete([FromQuery] Guid id)
+        {
+            return _service.Delete(id);
+        }
+
     }
 }
