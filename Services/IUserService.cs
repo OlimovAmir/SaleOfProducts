396785b7634811b5211e782ef1fd6f5cd5406445
olimov Amir <olimov.amir@mail.ru>
2024-02-18 16:28:51 +0300

    fix

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,7 +1,13 @@
-﻿namespace SaleOfProducts.Services
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
 {
     public interface IUserService
     {
-
+        IEnumerable<CashExpense> GetAll();
+        CashExpense GetById(Guid id);
+        string Create(CashExpense item);
+        string Update(Guid id, CashExpense item);
+        string Delete(Guid id);
     }
 }
