9977ca05de7a05827b13de864480c86e860feeda
olimov Amir <olimov.amir@mail.ru>
2024-02-18 16:34:19 +0300

    fix

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,9 +1,15 @@
 ﻿using SaleOfProducts.Models;
+using SaleOfProducts.Repositories;
 
 namespace SaleOfProducts.Services
 {
     public class UserService : IUserService
     {
+        IPostgreSQLRepository<User> _repository;
+        public UserService(IPostgreSQLRepository<User> repository)
+        {
+            _repository = repository;
+        }
         public string Create(User item)
         {
             throw new NotImplementedException();
