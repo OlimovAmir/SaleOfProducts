7b08e7901949681393787c0d9d6964332ed6f4e9
olimov Amir <olimov.amir@mail.ru>
2024-02-18 16:30:30 +0300

    fix

---
+++ Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class UserService : IUserService
+    {
+        public string Create(User item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, User item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
