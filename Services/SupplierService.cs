2bcde3823b8ef34f133d3a8b8d734568a9b93933
olimov Amir <olimov.amir@mail.ru>
2024-02-18 15:10:33 +0300

    fix bag

--- Services/SupplierService.cs
+++ Services/SupplierService.cs
@@ -17,13 +17,13 @@ namespace SaleOfProducts.Services
         {
             if (string.IsNullOrEmpty(item.Name))
             {
-                return "The description cannot be empty";
+                return "The name cannot be empty";
             }
-
-            _dbContext.Suppliers.Add(item);
-            _dbContext.SaveChanges();
-
-            return $"Created new item with this ID: {item.Id}";
+            else
+            {
+                _repository.Create(item);
+                return $"Created new item with this ID: {item.Id}";
+            }
         }
 
         public string Delete(Guid id)
