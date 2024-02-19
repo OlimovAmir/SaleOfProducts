489da792aa9272a4a2dc0ef990611765bae63c4e
olimov Amir <olimov.amir@mail.ru>
2024-02-19 12:57:30 +0300

    has been created file controller

---
+++ Controllers/ExpenseItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleOfProducts.Controllers
+{
+    public class ExpenseItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
