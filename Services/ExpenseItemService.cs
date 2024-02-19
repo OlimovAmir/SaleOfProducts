067c5fc3bf81a14b5db9f533172ccad156e2893f
olimov Amir <olimov.amir@mail.ru>
2024-02-19 12:45:43 +0300

    fix

---
+++ Services/ExpenseItemService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class ExpenseItemService : IExpenseItemService
+    {
+        public string Create(ExpenseItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ExpenseItem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ExpenseItem GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, ExpenseItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
