1e87655b8c68914cd5abe29232fa7961737abfe6
olimov Amir <olimov.amir@mail.ru>
2024-02-19 12:44:54 +0300

    fix

---
+++ Services/IExpenseItemService.cs
@@ -0,0 +1,13 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public interface IExpenseItemService
+    {
+        IEnumerable<ExpenseItem> GetAll();
+        ExpenseItem GetById(Guid id);
+        string Create(ExpenseItem item);
+        string Update(Guid id, ExpenseItem item);
+        string Delete(Guid id);
+    }
+}
