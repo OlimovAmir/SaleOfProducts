122d53d81c1d6c37aa0ea2428d90123cc90fdb91
olimov Amir <olimov.amir@mail.ru>
2024-02-19 12:42:10 +0300

    fix

--- Models/ExpenseItem.cs
+++ Models/ExpenseItem.cs
@@ -4,6 +4,24 @@ namespace SaleOfProducts.Models
 {
     public class ExpenseItem : BaseEntity
     {
+        public Guid Id { get; }
+        public string Name { get; set; }
 
+
+        // Constructor
+        public ExpenseItem(string name)
+        {
+            Name = name;
+        }
+
+        public ExpenseItem()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
 }
