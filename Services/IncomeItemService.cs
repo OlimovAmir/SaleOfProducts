523ef5b6ddaee1e95f60b2a9ee764669c773df96
olimov Amir <olimov.amir@mail.ru>
2024-02-20 12:15:06 +0300

    fix

--- Services/IncomeItemService.cs
+++ Services/IncomeItemService.cs
@@ -1,6 +1,32 @@
-﻿namespace SaleOfProducts.Services
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
 {
-    public class IncomeItemService
+    public class IncomeItemService : IIncomeItemService
     {
+        public string Create(IncomeItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IncomeItem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IncomeItem GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, IncomeItem item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
