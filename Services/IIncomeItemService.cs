c30811cf74f2e245e94b00c9342926599a7f0e1c
olimov Amir <olimov.amir@mail.ru>
2024-02-20 12:13:17 +0300

    fix

--- Services/IIncomeItemService.cs
+++ Services/IIncomeItemService.cs
@@ -1,7 +1,13 @@
-﻿namespace SaleOfProducts.Services
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
 {
     public interface IIncomeItemService
     {
-
+        IEnumerable<IncomeItem> GetAll();
+        IncomeItem GetById(Guid id);
+        string Create(IncomeItem item);
+        string Update(Guid id, IncomeItem item);
+        string Delete(Guid id);
     }
 }
