9dc1008ea589754e2114be2ed25295ae90763565
olimov Amir <olimov.amir@mail.ru>
2024-02-20 12:26:57 +0300

    fix

--- Controllers/IncomeItemController.cs
+++ Controllers/IncomeItemController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProducts.Models;
+using SaleOfProducts.Services;
 
 namespace SaleOfProducts.Controllers
 {
@@ -6,9 +8,41 @@ namespace SaleOfProducts.Controllers
     [Route("[controller]")]
     public class IncomeItemController : Controller
     {
-        public IActionResult Index()
+        readonly IExpenseItemService _service;
+
+        public ExpenseItemController(IExpenseItemService service)
         {
-            return View();
+            _service = service;
+        }
+
+        [HttpGet("AllItems")]
+        public IEnumerable<ExpenseItem> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("GetItemById")]
+        public ExpenseItem Get(Guid id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpPost("Create")]
+        public string Post([FromBody] ExpenseItem item)
+        {
+            return _service.Create(item);
+        }
+
+        [HttpPut("Update")]
+        public string Put([FromQuery] Guid id, [FromBody] ExpenseItem item)
+        {
+            return _service.Update(id, item);
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete([FromQuery] Guid id)
+        {
+            return _service.Delete(id);
         }
     }
 }
