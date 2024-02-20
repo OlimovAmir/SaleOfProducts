7b686f4b29b5258dd104681275b3caa9b83daca0
olimov Amir <olimov.amir@mail.ru>
2024-02-20 12:10:47 +0300

    fix

--- Models/IncomeItem.cs
+++ Models/IncomeItem.cs
@@ -4,6 +4,24 @@ namespace SaleOfProducts.Models
 {
     public class IncomeItem : BaseEntity
     {
+        public Guid Id { get; }
+        public string Name { get; set; }
 
+
+        // Constructor
+        public IncomeItem(string name)
+        {
+            Name = name;
+        }
+
+        public IncomeItem()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
 }
