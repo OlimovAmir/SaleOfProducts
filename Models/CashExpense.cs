7994b25faf4bdcbfb41af3a602a7894120a2a214
olimov Amir <olimov.amir@mail.ru>
2024-02-20 13:47:51 +0300

    fix bag

--- Models/CashExpense.cs
+++ Models/CashExpense.cs
@@ -5,23 +5,22 @@ namespace SaleOfProducts.Models
     public class CashExpense : CashTransaction
     {
         // Дополнительные свойства для учета расходов, если необходимо
-        public string Category { get; set; }
+        
         public Guid PositionId { get; set; } // Идентификатор должности
         public ExpenseItem ExpenseItem { get; set; } // Ссылка на должность
 
         // Конструктор
-        public CashExpense(double amount, string description, string category)
+        public CashExpense(double amount, string description, ExpenseItem expenseItem)
             : base(amount, description)
         {
             Amount = amount;
-            Category = category;
             Description = description;
-            Category = category;
+            ExpenseItem = expenseItem;
         }
 
         public override string ToString()
         {
-            return $"{base.ToString()}, Category: {Category}";
+            return $"{base.ToString()}, Category: {Description} {ExpenseItem}";
         }
     }
 }
