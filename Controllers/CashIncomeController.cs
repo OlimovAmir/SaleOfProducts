7a6a552069f90a4bdfdfdf0d553267dc42820bfa
olimov Amir <olimov.amir@mail.ru>
2024-02-22 15:09:14 +0300

    fix

--- Controllers/CashIncomeController.cs
+++ Controllers/CashIncomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProducts.Models;
 using SaleOfProducts.Services;
 
 namespace SaleOfProducts.Controllers
@@ -12,6 +13,36 @@ namespace SaleOfProducts.Controllers
         public CashIncomeController(ICashIncomeService service)
         {
             _service = service;
+        }
+
+        [HttpGet("AllItems")]
+        public IEnumerable<CashExpense> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("GetItemById")]
+        public CashExpense Get(Guid id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpPost("Create")]
+        public string Post([FromBody] CashExpense item)
+        {
+            return _service.Create(item);
+        }
+
+        [HttpPut("Update")]
+        public string Put([FromQuery] Guid id, [FromBody] CashExpense item)
+        {
+            return _service.Update(id, item);
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete([FromQuery] Guid id)
+        {
+            return _service.Delete(id);
         }
     }
 }
