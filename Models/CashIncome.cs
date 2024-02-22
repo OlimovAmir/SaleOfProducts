2bca61339101fd29dbf66926119aa3f5bf828c03
olimov Amir <olimov.amir@mail.ru>
2024-02-22 14:57:43 +0300

    fix

--- Models/CashIncome.cs
+++ Models/CashIncome.cs
@@ -8,10 +8,9 @@ namespace SaleOfProducts.Models
         public ICollection<IncomeItem> IncomeItems { get; set; } // Ссылка на список групп расходов
 
         // Конструктор
-        public CashIncome(double amount, string description)
-            : base(amount, description)
+        public CashIncome(double amount, string description) : base(amount, description)
         {
-            
+            IncomeItems = new List<IncomeItem>();
         }
 
         public override string ToString()
