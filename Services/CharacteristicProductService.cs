70a6d22888b88f1ff7b6fc33bd1d466c12796d65
olimov Amir <olimov.amir@mail.ru>
2024-02-24 20:47:03 +0300

    fix

--- Services/CharacteristicProductService.cs
+++ Services/CharacteristicProductService.cs
@@ -1,7 +1,32 @@
-﻿namespace SaleOfProducts.Services
-{
-    public class CharacteristicProductService
-    {
+﻿using SaleOfProducts.Models;
 
+namespace SaleOfProducts.Services
+{
+    public class CharacteristicProductService : ICharacteristicProductService
+    {
+        public string Create(CharacteristicProduct item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<CharacteristicProduct> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CharacteristicProduct GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, CharacteristicProduct item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
