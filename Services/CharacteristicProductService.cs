7e7c67d8d9eeead3736589fa630a04c6095f6e2a
olimov Amir <olimov.amir@mail.ru>
2024-02-24 20:59:52 +0300

    fix

--- Services/CharacteristicProductService.cs
+++ Services/CharacteristicProductService.cs
@@ -40,7 +40,7 @@ namespace SaleOfProducts.Services
 
         public CharacteristicProduct GetById(Guid id)
         {
-            throw new NotImplementedException();
+            return _repository.GetById(id);
         }
 
         public string Update(Guid id, CharacteristicProduct item)
