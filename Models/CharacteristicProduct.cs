74fe2f8807539cd19bd97409db3af7780f802234
olimov Amir <olimov.amir@mail.ru>
2024-02-24 20:41:36 +0300

    fix

--- Models/CharacteristicProduct.cs
+++ Models/CharacteristicProduct.cs
@@ -5,20 +5,17 @@ namespace SaleOfProducts.Models
 {
     public class CharacteristicProduct : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public List<string> Colors { get; set; }
-        public List<string> Sizes { get; set; }
+       
 
         [Column(TypeName = "jsonb")] // для PostgreSQL, в других СУБД используйте "json"
         public Dictionary<string, string> Specifications { get; set; }
 
         public CharacteristicProduct()
         {
-            Colors = new List<string>();
-            Sizes = new List<string>();
+            
             Specifications = new Dictionary<string, string>();
         }
     }
