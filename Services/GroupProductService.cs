7d06a4f24880d1e08a771f977b82f32650d8ef41
olimov Amir <olimov.amir@mail.ru>
2024-02-25 19:28:15 +0300

    fix

--- Services/GroupProductService.cs
+++ Services/GroupProductService.cs
@@ -1,6 +1,32 @@
-﻿namespace SaleOfProducts.Services
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
 {
-    public class GroupProductService
+    public class GroupProductService : IGroupProductService
     {
+        public string Create(GroupProduct item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<GroupProduct> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GroupProduct GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, GroupProduct item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
