bea78db81453dbec09f6edc632f2491b5907c0b0
olimov Amir <olimov.amir@mail.ru>
2024-02-25 19:32:01 +0300

    fix

--- Services/GroupProductService.cs
+++ Services/GroupProductService.cs
@@ -40,7 +40,7 @@ namespace SaleOfProducts.Services
 
         public GroupProduct GetById(Guid id)
         {
-            throw new NotImplementedException();
+            return _repository.GetById(id);
         }
 
         public string Update(Guid id, GroupProduct item)
