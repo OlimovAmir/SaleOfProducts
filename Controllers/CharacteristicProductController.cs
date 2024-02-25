487afe0d5f7be9c1b5b6cd89dc66874416abd01d
olimov Amir <olimov.amir@mail.ru>
2024-02-25 12:58:52 +0300

    has been created file controller

---
+++ Controllers/CharacteristicProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleOfProducts.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacteristicProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
