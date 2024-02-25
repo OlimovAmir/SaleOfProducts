41dd7320cea073c27a27e0edf496f792eaa3f8a4
olimov Amir <olimov.amir@mail.ru>
2024-02-25 19:22:49 +0300

    fix

--- Models/GroupProduct.cs
+++ Models/GroupProduct.cs
@@ -1,6 +1,24 @@
-﻿namespace SaleOfProducts.Models
+﻿using SaleOfProducts.Models.BaseClassModels;
+
+namespace SaleOfProducts.Models
 {
-    public class GroupProduct
+    public class GroupProduct : BaseEntity
     {
+        public string Name { get; set; }
+
+        public GroupProduct(string name)
+        {
+            Name = name;
+        }
+
+        public GroupProduct()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
     }
 }
