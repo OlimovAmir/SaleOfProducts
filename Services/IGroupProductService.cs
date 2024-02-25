fab04da4e6285186a85eabc5a996d53ffb50de0f
olimov Amir <olimov.amir@mail.ru>
2024-02-25 19:27:08 +0300

    fix

--- Services/IGroupProductService.cs
+++ Services/IGroupProductService.cs
@@ -1,7 +1,13 @@
-﻿namespace SaleOfProducts.Services
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
 {
     public interface IGroupProductService
     {
-
+        IQueryable<GroupProduct> GetAll();
+        GroupProduct GetById(Guid id);
+        string Create(GroupProduct item);
+        string Update(Guid id, GroupProduct item);
+        string Delete(Guid id);
     }
 }
