1b2185a75b57decf13a7a0a274b12dc7d1b31547
olimov Amir <olimov.amir@mail.ru>
2024-02-25 13:01:16 +0300

    fix

--- Controllers/CharacteristicProductController.cs
+++ Controllers/CharacteristicProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProducts.Models;
 using SaleOfProducts.Services;
 
 namespace SaleOfProducts.Controllers
@@ -12,6 +13,36 @@ namespace SaleOfProducts.Controllers
         public CharacteristicProductController(ICharacteristicProductService service)
         {
             _service = service;
+        }
+
+        [HttpGet("AllItems")]
+        public IEnumerable<CharacteristicProduct> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("GetItemById")]
+        public CharacteristicProduct Get(Guid id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpPost("Create")]
+        public string Post([FromForm] CharacteristicProduct item)
+        {
+            return _service.Create(item);
+        }
+
+        [HttpPut("Update")]
+        public string Put([FromQuery] Guid id, [FromBody] CharacteristicProduct item)
+        {
+            return _service.Update(id, item);
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete([FromQuery] Guid id)
+        {
+            return _service.Delete(id);
         }
     }
 }
