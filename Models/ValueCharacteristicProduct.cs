ecbd92ec38eb35dfd1ba2be4e45344f364264410
olimov Amir <olimov.amir@mail.ru>
2024-02-26 21:40:57 +0300

    fix

---
+++ Models/ValueCharacteristicProduct.cs
@@ -0,0 +1,24 @@
+﻿using SaleOfProducts.Models.BaseClassModels;
+
+namespace SaleOfProducts.Models
+{
+    public class NameCharacteristicProduct : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public NameCharacteristicProduct(string name)
+        {
+            Name = name;
+        }
+
+        public NameCharacteristicProduct()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
