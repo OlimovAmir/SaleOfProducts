c89a0adcfef2758b2c9519e465f67fcc130f18dc
olimov Amir <olimov.amir@mail.ru>
2024-02-28 19:52:29 +0300

    fix

--- Controllers/ValueCharacteristicProductController.cs
+++ Controllers/ValueCharacteristicProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SaleOfProducts.Models;
+using SaleOfProducts.Services;
 
 namespace SaleOfProducts.Controllers
 {
@@ -6,9 +8,40 @@ namespace SaleOfProducts.Controllers
     [ApiController]
     public class ValueCharacteristicProductController : Controller
     {
-        public IActionResult Index()
+        readonly INameCharacteristicProductService _service;
+
+        public NameCharacteristicProductController(NameCharacteristicProductService service)
         {
-            return View();
+            _service = service;
+        }
+        [HttpGet("AllItems")]
+        public IQueryable<NameCharacteristicProduct> Get()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("GetItemById")]
+        public NameCharacteristicProduct Get(Guid id)
+        {
+            return _service.GetById(id);
+        }
+
+        [HttpPost("Create")]
+        public string Post([FromBody] NameCharacteristicProduct item)
+        {
+            return _service.Create(item);
+        }
+
+        [HttpPut("Update")]
+        public string Put([FromQuery] Guid id, [FromBody] NameCharacteristicProduct item)
+        {
+            return _service.Update(id, item);
+        }
+
+        [HttpDelete("Delete")]
+        public string Delete([FromQuery] Guid id)
+        {
+            return _service.Delete(id);
         }
     }
 }
