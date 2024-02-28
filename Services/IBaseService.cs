148707978ef32e38390d5fb2e037c64f3277449f
olimov Amir <olimov.amir@mail.ru>
2024-02-28 20:14:25 +0300

    fix

---
+++ Services/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using SaleOfProducts.Models.BaseClassModels;
+
+namespace SaleOfProducts.Services
+{
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetAll();
+
+        TEntity GetById(Guid id);
+
+        string Create(TEntity worker);
+
+        string Update(Guid id, TEntity item);
+
+        string Delete(Guid id);
+    }
+}
