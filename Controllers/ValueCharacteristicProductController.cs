d49dca190bd92b83031bc6aff280e2b073f18365
olimov Amir <olimov.amir@mail.ru>
2024-02-28 19:51:15 +0300

    fix

---
+++ Controllers/ValueCharacteristicProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleOfProducts.Controllers
+{
+    public class ValueCharacteristicProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
