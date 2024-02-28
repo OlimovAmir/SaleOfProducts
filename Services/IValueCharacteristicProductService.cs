3c12e7368da1d2c0f7ff6bbcafada484872ad73b
olimov Amir <olimov.amir@mail.ru>
2024-02-28 21:12:28 +0300

    fix

--- Services/IValueCharacteristicProductService.cs
+++ Services/IValueCharacteristicProductService.cs
@@ -2,12 +2,7 @@
 
 namespace SaleOfProducts.Services
 {
-    public interface IValueCharacteristicProductService
+    public interface IValueCharacteristicProductService : IBaseService<ValueCharacteristicProduct>
     {
-        IQueryable<ValueCharacteristicProduct> GetAll();
-        ValueCharacteristicProduct GetById(Guid id);
-        string Create(ValueCharacteristicProduct item);
-        string Update(Guid id, ValueCharacteristicProduct item);
-        string Delete(Guid id);
     }
 }
