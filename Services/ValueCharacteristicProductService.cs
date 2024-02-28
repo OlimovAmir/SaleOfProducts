7223fb6d3cad4ffd8d39b046e7b8aff93ce4e243
olimov Amir <olimov.amir@mail.ru>
2024-02-28 13:45:33 +0300

    fix

---
+++ Services/ValueCharacteristicProductService.cs
@@ -0,0 +1,32 @@
+﻿using SaleOfProducts.Models;
+
+namespace SaleOfProducts.Services
+{
+    public class ValueCharacteristicProductService : IValueCharacteristicProductService
+    {
+        public string Create(ValueCharacteristicProduct item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<ValueCharacteristicProduct> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueCharacteristicProduct GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Update(Guid id, ValueCharacteristicProduct item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
