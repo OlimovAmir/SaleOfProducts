bf493f7b020f0e940ba1954b1eb354bc6dc07cf0
olimov Amir <olimov.amir@mail.ru>
2024-02-28 21:08:28 +0300

    fix

--- Services/IPositionService.cs
+++ Services/IPositionService.cs
@@ -2,12 +2,7 @@
 
 namespace SaleOfProducts.Services
 {
-    public interface IPositionService
+    public interface IPositionService : IBaseService<Position>
     {
-        IEnumerable<Position> GetAll();
-        Position GetById(Guid id);
-        string Create(Position item);
-        string Update(Guid id, Position item);
-        string Delete(Guid id);
     }
 }
