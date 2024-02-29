f76db4bd19db0a7ef149a724ea9f140d6ccf794a
olimov Amir <olimov.amir@mail.ru>
2024-02-29 11:23:11 +0300

    has been created base controller

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaleOfProducts.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
