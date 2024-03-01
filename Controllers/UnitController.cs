5f7931750e7910620a2ada0a60bc040b23ad2406
olimov Amir <olimov.amir@mail.ru>
2024-03-01 21:57:11 +0300

    has been modified

--- Controllers/UnitController.cs
+++ Controllers/UnitController.cs
@@ -8,42 +8,10 @@ namespace SaleOfProducts.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UnitController : ControllerBase
+    public class UnitController : BaseController<Unit>
     {
-        readonly IUnitService _service;
-
-        public UnitController(IUnitService service)
+        public UnitController(ILogger<UnitController> logger, IUnitService service) : base(logger, service)
         {
-            _service = service;
-        }
-        [HttpGet("AllItems")]
-        public IEnumerable<Unit> Get()
-        {
-            return _service.GetAll();
-        }
-
-        [HttpGet("GetItemById")]
-        public Unit Get(Guid id)
-        {
-            return _service.GetById(id);
-        }
-
-        [HttpPost("Create")]
-        public string Post([FromBody] Unit item)
-        {
-            return _service.Create(item);
-        }
-
-        [HttpPut("Update")]
-        public string Put([FromQuery] Guid id, [FromBody] Unit  item)
-        {
-            return _service.Update(id, item);
-        }
-
-        [HttpDelete("Delete")]
-        public string Delete([FromQuery] Guid id)
-        {
-            return _service.Delete(id);
         }
     }
 }
