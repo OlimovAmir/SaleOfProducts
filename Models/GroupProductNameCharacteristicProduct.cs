656f3e286732270a4ff4adde315f2105f16a0569
olimov Amir <olimov.amir@mail.ru>
2024-03-14 09:51:25 +0300

    fix

--- Models/GroupProductNameCharacteristicProduct.cs
+++ Models/GroupProductNameCharacteristicProduct.cs
@@ -4,6 +4,13 @@ namespace SaleOfProducts.Models
 {
     public class GroupProductNameCharacteristicProduct : BaseEntity
     {
+        public int NameCharacteristicProductId { get; set; }
+        public NameCharacteristicProduct NameCharacteristicProduct { get; set; }
+
+        public int GroupProductId { get; set; }
+        public GroupProduct GroupProduct { get; set; }
+
+
 
     }
 }
