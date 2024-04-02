605f27dbdf98b2222174b1adcb12849e9f46d301
olimov Amir <olimov.amir@mail.ru>
2024-04-02 16:22:42 +0300

    fix bag

--- Models/NameCharacteristicProduct.cs
+++ Models/NameCharacteristicProduct.cs
@@ -9,6 +9,7 @@ namespace SaleOfProducts.Models
         [StringLength(100, MinimumLength = 1, ErrorMessage = "Длина названия должна быть от 1 до 100 символов")]
         //[RegularExpression(@"^[A-Za-z0-9\s]+$", ErrorMessage = "Название должно содержать только буквы, цифры и пробелы")]
         public string Name { get; set; }
+
         public ICollection<GroupProductNameCharacteristicProduct> GroupProductCharacteristics { get; set; }
 
         public NameCharacteristicProduct(string name, ICollection<GroupProductNameCharacteristicProduct> groupProductCharacteristics)
