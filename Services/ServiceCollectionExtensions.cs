047acd0e2ad69ecda91ca9f11efa6c1006b59e6e
olimov Amir <olimov.amir@mail.ru>
2024-04-09 12:20:53 +0300

    has been created class ServiceCollectionExtensions

---
+++ Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using SaleOfProducts.Repositories;
+
+namespace SaleOfProducts.Services
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddMyServices(this IServiceCollection service)
+        {
+            //service.AddScoped<AuthService>();
+
+
+            service.AddScoped(typeof(IPostgreSQLRepository<>), typeof(PostgreSQLRepository<>));
+        }
+    }
+}
