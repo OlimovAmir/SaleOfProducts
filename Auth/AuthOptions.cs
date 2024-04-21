26901c02b2aa81750082f7eccd68077d320229b3
olimov Amir <olimov.amir@mail.ru>
2024-04-21 13:19:28 +0300

    fix

--- Auth/AuthOptions.cs
+++ Auth/AuthOptions.cs
@@ -1,6 +1,15 @@
-﻿namespace SaleOfProducts.Auth
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SaleOfProducts.Auth
 {
     public class AuthOptions
     {
+        public const string ISSUER = "SaleOfProductJWT";
+        public const string AUDIENCE = "MyAuthUser"; // потребитель токена
+        const string KEY = "2EC1EE51-1100-4347-BF22-EEB6CB8B0695";   // ключ для шифрации
+        public const int LIFETIME = 30; // время жизни токена - 15 минута
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
     }
 }
