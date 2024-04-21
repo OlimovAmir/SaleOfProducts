c65cf8a5d734c4a7f4c324ed61b0947eb05cb9c2
olimov Amir <olimov.amir@mail.ru>
2024-04-21 13:21:12 +0300

    fix

--- Auth/ServiceCollectionExtensions.cs
+++ Auth/ServiceCollectionExtensions.cs
@@ -1,6 +1,47 @@
-﻿namespace SaleOfProducts.Auth
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SaleOfProducts.Auth
 {
-    public class ServiceCollectionExtensions
+    public static class ServiceCollectionExtensions
     {
+        public static void AddMyAuth(this IServiceCollection service)
+        {
+            service.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminOnly",
+                    policy =>
+                    {
+                        policy.RequireRole("admin");
+                        policy.RequireRole("editor");
+                    });
+                options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+            });
+
+            service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // указывает, будет ли валидироваться издатель при валидации токена
+                        ValidateIssuer = true,
+                        // строка, представляющая издателя
+                        ValidIssuer = AuthOptions.ISSUER,
+
+                        // будет ли валидироваться потребитель токена
+                        ValidateAudience = true,
+                        // установка потребителя токена
+                        ValidAudience = AuthOptions.AUDIENCE,
+
+                        // будет ли валидироваться время существования
+                        ValidateLifetime = true,
+
+                        // валидация ключа безопасности
+                        ValidateIssuerSigningKey = true,
+                        // установка ключа безопасности
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                    };
+                });
+        }
     }
 }
