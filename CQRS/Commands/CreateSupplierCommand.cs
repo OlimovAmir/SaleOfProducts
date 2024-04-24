34ad4f2fc6a4c6ea90286c29839f688e09406651
olimov Amir <olimov.amir@mail.ru>
2024-04-24 15:04:48 +0300

    fix

---
+++ CQRS/Commands/CreateSupplierCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace SaleOfProducts.CQRS.Commands
+{
+    public class CreateSupplierCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
