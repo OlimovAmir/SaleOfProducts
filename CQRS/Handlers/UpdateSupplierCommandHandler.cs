6ba6a6ee0a3ec9407973c4bf3167dd553d1a2868
olimov Amir <olimov.amir@mail.ru>
2024-04-25 12:00:07 +0300

    fix

--- CQRS/Handlers/UpdateSupplierCommandHandler.cs
+++ CQRS/Handlers/UpdateSupplierCommandHandler.cs
@@ -1,8 +1,28 @@
-﻿
+﻿using AutoMapper;
+using MediatR;
+using SaleOfProducts.CQRS.Commands;
+using SaleOfProducts.Models;
+using SaleOfProducts.Services.IService;
 
 namespace SaleOfProducts.CQRS.Handlers
 {
-    public class UpdateSupplierCommandHandler
+    public class UpdateSupplierCommandHandler : IRequestHandler<UpdateSupplierCommand, string>
     {
+        private ISupplierService _service;
+        private readonly IMapper _mapper;
+
+        public UpdateSupplierCommandHandler(ISupplierService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public Task<string> Handle(UpdateSupplierCommand request, CancellationToken cancellationToken)
+        {
+            var client = _mapper.Map<Supplier>(request);
+            var result = _service.Update(request.Id, client);
+
+            return Task.FromResult(result);
+        }
     }
 }
