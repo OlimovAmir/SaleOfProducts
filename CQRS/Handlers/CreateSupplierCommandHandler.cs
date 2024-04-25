e59b324ed46b3611d99d454b2a1c58a65699bcbc
olimov Amir <olimov.amir@mail.ru>
2024-04-25 10:53:04 +0300

    has been created class CreateSupplierCommandHandler

---
+++ CQRS/Handlers/CreateSupplierCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using SaleOfProducts.CQRS.Commands;
+using SaleOfProducts.Models;
+using SaleOfProducts.Services.IService;
+
+namespace SaleOfProducts.CQRS.Handlers
+{
+    public class CreateSupplierCommandHandler : IRequestHandler<CreateSupplierCommand, Supplier>
+    {
+        private ISupplierService _service;
+        private readonly IMapper _mapper;
+
+        public CreateSupplierCommandHandler(ISupplierService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public Task<Supplier> Handle(CreateSupplierCommand request, CancellationToken cancellationToken)
+        {
+            var user = _mapper.Map<Supplier>(request);
+            _service.Create(user);
+            return Task.FromResult(user);
+        }
+    }
+}
