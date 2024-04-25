fc76e7c3190fbf764aae5bb6a51c2f8c04ec11ee
olimov Amir <olimov.amir@mail.ru>
2024-04-25 11:07:57 +0300

    fix

--- CQRS/Commands/UpdateSupplierCommand.cs
+++ CQRS/Commands/UpdateSupplierCommand.cs
@@ -1,9 +1,13 @@
 ﻿using MediatR;
+using System.Text.Json.Serialization;
 
 namespace SaleOfProducts.CQRS.Commands
 {
     public class UpdateSupplierCommand : IRequest<string>
     {
-
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
