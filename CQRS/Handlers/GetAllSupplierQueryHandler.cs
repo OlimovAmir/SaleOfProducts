1f44ffd2c4e342c1be8267e160c6ed2bbb11a13e
olimov Amir <olimov.amir@mail.ru>
2024-04-25 11:15:57 +0300

    fix

--- CQRS/Handlers/GetAllSupplierQueryHandler.cs
+++ CQRS/Handlers/GetAllSupplierQueryHandler.cs
@@ -1,8 +1,25 @@
-﻿
+﻿using AutoMapper;
+using MediatR;
+using SaleOfProducts.Models;
+using SaleOfProducts.Services.IService;
 
 namespace SaleOfProducts.CQRS.Handlers
 {
-    public class GetAllSupplierQueryHandler
+    public class GetAllSupplierQueryHandler : IRequestHandler<GetAllSuppliersQuery, List<Supplier>>
     {
+        private ISupplierService _service;
+        private readonly IMapper _mapper;
+
+        public GetAllSupplierQueryHandler(ISupplierService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        public async Task<List<Supplier>> Handle(GetAllSuppliersQuery request, CancellationToken cancellationToken)
+        {
+            var clients = _service.GetAll().ToList();
+            return await Task.FromResult(clients);
+        }
     }
 }
