4df3d3980e9f4e68b9a899dc9bbfc0fb978a7e4d
olimov Amir <olimov.amir@mail.ru>
2024-05-11 08:36:43 +0300

    fix bag

--- Models/GroupProduct.cs
+++ Models/GroupProduct.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using SaleOfProducts.Models.BaseClassModels;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SaleOfProducts.Models
 {
     public class GroupProduct : BaseEntity
     {
+        [Key]
+        [Column("ProductId")]
         public Guid Id { get; set; } // Добавляем идентификатор группы продуктов
 
         [Required(ErrorMessage = "Пожалуйста, введите название характеристики продукта")]
         [StringLength(100, MinimumLength = 1, ErrorMessage = "Длина названия должна быть от 1 до 100 символов")]
         //[RegularExpression(@"^[A-Za-z0-9\s]+$", ErrorMessage = "Название должно содержать только буквы, цифры и пробелы")]
         public string Name { get; set; }
+        
+        public ICollection<NameCharacteristicProduct> NameCharacteristicProducts { get; set; }
 
-        [JsonIgnore]
-        public ICollection<Product>? Products { get; set; }
+
 
         public GroupProduct(string name, Guid id)
         {
--- Models/NameCharacteristicProduct.cs
+++ Models/NameCharacteristicProduct.cs
@@ -1,29 +1,32 @@
 ﻿using SaleOfProducts.Models.BaseClassModels;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SaleOfProducts.Models
 {
     public class NameCharacteristicProduct : BaseEntity
     {
+        [Key]
+        [Column("ProductGroupId")]
+        public override Guid Id { get; set; }
+
         [Required(ErrorMessage = "Пожалуйста, введите название характеристики продукта")]
         [StringLength(100, MinimumLength = 1, ErrorMessage = "Длина названия должна быть от 1 до 100 символов")]
         //[RegularExpression(@"^[A-Za-z0-9\s]+$", ErrorMessage = "Название должно содержать только буквы, цифры и пробелы")]
-        public string Name { get; set; }
 
-        public ICollection<GroupProductNameCharacteristicProduct> GroupProductCharacteristics { get; set; }
 
-        public NameCharacteristicProduct(string name, ICollection<GroupProductNameCharacteristicProduct> groupProductCharacteristics)
-        {
-            
-            Name = name;
-            GroupProductCharacteristics = groupProductCharacteristics;
+        public string Name { get; set; }        
+
+        public NameCharacteristicProduct(string name)
+        {            
+            Name = name;            
         }
 
         public NameCharacteristicProduct() { }
 
         public override string ToString()
         {
-            return $"{Name} {GroupProductCharacteristics} ";
+            return $"{Name}";
         }
     }
 }
