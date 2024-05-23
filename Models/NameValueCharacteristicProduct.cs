ff8ba2197042e4b5af99d412c1cabf173777de9a
olimov Amir <olimov.amir@mail.ru>
2024-05-23 11:24:58 +0300

    fix

--- Models/NameValueCharacteristicProduct.cs
+++ Models/NameValueCharacteristicProduct.cs
@@ -2,6 +2,10 @@
 {
     public class NameValueCharacteristicProduct
     {
+        public Guid NameCharacteristicProductId { get; set; }
+        public NameCharacteristicProduct NameCharacteristicProduct { get; set; }
 
+        public Guid ValueCharacteristicProductId { get; set; }
+        public ValueCharacteristicProduct ValueCharacteristicProduct { get; set; }
     }
 }
