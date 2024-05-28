ec456bcbb27769a4a09c7b848e762116526d5bb1
olimov Amir <olimov.amir@mail.ru>
2024-05-28 10:53:29 +0300

    fix bag

--- Services/Service/PurchaseProductService.cs
+++ Services/Service/PurchaseProductService.cs
@@ -15,27 +15,55 @@ namespace SaleOfProducts.Services.Service
 
         public string Create(PurchaseProduct item)
         {
-            throw new NotImplementedException();
+            if (item.GroupProduct == null || string.IsNullOrEmpty(item.GroupProduct.Name))
+            {
+                return "The name cannot be empty";
+            }
+            else
+            {
+                _repository.Create(item);
+                return $"Created new item with this ID: {item.Id}";
+            }
         }
 
         public string Delete(Guid id)
         {
-            throw new NotImplementedException();
+            var result = _repository.Delete(id);
+            if (result)
+                return "Item deleted";
+            else
+                return "Item not found";
         }
 
         public IQueryable<PurchaseProduct> GetAll()
         {
-            throw new NotImplementedException();
+            return _repository.GetAll();
         }
 
         public PurchaseProduct GetById(Guid id)
         {
-            throw new NotImplementedException();
+            return _repository.GetById(id);
         }
 
         public string Update(Guid id, PurchaseProduct item)
         {
-            throw new NotImplementedException();
+            var _item = _repository.GetById(id);
+            if (_item is not null)
+            {
+                _item.Price = item.Price;
+                _item.PurchaseDate = item.PurchaseDate;
+                _item.Quantity = item.Quantity;
+                _item.Unit = item.Unit;
+                _item.GroupProduct = item.GroupProduct;
+                _item.Supplier = item.Supplier;
+                _item.ValueCharacteristicProducts = item.ValueCharacteristicProducts;
+
+                var result = _repository.Update(_item);
+                if (result)
+                    return "Item updated";
+            }
+
+            return "Item updated";
         }
     }
 }
