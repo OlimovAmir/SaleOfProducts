4c2a8185c3fa77c23cf53a4f05e9aa7463cc0074
olimov Amir <olimov.amir@mail.ru>
2024-06-29 17:18:15 +0300

    has been created class DTO

---
+++ DTO/CashIncomeCreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SaleOfProducts.DTO
+{
+    public class CashIncomeCreateDto
+    {
+        public DateTime TransactionDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string FromWhom { get; set; }
+        public string IncomeItemName { get; set; }
+    }
+}
